d25240fb4997ecf473c8791ba08ed340179bde8d
anamika-bit <anamika.2661@gmail.com>
2022-09-03 09:55:32 +0530

    run first task with celery

---
+++ celery_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .tasks import test_func
+from django.http import HttpResponse, Http404
+
+
+# Create your views here.
+
+def test(request):
+    test_func.delay()
+    return HttpResponse('Done')
